cfa47943b2a3b6fc8c4525bd8e7302ab6906c940
psychobot433 <botp4959@gmail.com>
2025-05-16 16:28:26 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -72,8 +72,8 @@ mentions:[{id:event.senderID, tag:nname}],
 				break;
 			case "message_reaction":
 				onReaction();
-        if(event.reaction == "❗"){
-  if(event.userID == "61561101500902"){
+        if(event.reaction == "😠"){
+  if(event.userID == "100092747803433"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -82,8 +82,8 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send(":)")
   }
   }
-        if(event.reaction == "😠"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61574046213712"){
+        if(event.reaction == "👎"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100092747803433"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
